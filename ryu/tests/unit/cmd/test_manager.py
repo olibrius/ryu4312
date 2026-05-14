26423ed11ed9522691da4bd0a20cf13ab3c538dd
Olivier Tirat <olivier.tirat@free.fr>
2026-05-14 11:15:27 +0200

    fix: remove unused import sys in test_manager.py

--- ryu/tests/unit/cmd/test_manager.py
+++ ryu/tests/unit/cmd/test_manager.py
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
 import unittest
 from importlib import reload
 from unittest import mock
