102f3d93382d84be42dcb453a520376a39321ee3
Olivier Tirat <olivier.tirat@free.fr>
2026-05-14 11:11:52 +0200

    fix: simplify hooks.py — remove easy_install.get_script_args hack (removed in modern setuptools)

--- ryu/hooks.py
+++ ryu/hooks.py
@@ -1,5 +1,3 @@
-# vim: tabstop=4 shiftwidth=4 softtabstop=4
-
 # Copyright 2013 Hewlett-Packard Development Company, L.P.
 # All Rights Reserved.
 #
@@ -15,25 +13,12 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import os
 import sys
-from setuptools.command import easy_install
 from ryu import version
 
 
-# Global variables in this module doesn't work as we expect
-# because, during the setup procedure, this module seems to be
-# copied (as a file) and can be loaded multiple times.
-# We save them into __main__ module instead.
-def _main_module():
-    return sys.modules['__main__']
-
-
 def save_orig():
-    """Save original easy_install.get_script_args.
-    This is necessary because pbr's setup_hook is sometimes called
-    before ours."""
-    _main_module()._orig_get_script_args = easy_install.get_script_args
+    pass
 
 
 def setup_hook(config):
@@ -43,28 +28,4 @@ def setup_hook(config):
         requires = metadata.get('requires_dist', '').split('\n')
         metadata['requires_dist'] = "\n".join(requires)
     config['metadata'] = metadata
-
     metadata['version'] = str(version)
-
-    # pbr's setup_hook replaces easy_install.get_script_args with
-    # their own version, override_get_script_args, prefering simpler
-    # scripts which are not aware of multi-version.
-    # prevent that by doing the opposite.  it's a horrible hack
-    # but we are in patching wars already...
-    from pbr import packaging
-
-    def my_get_script_args(*args, **kwargs):
-        return _main_module()._orig_get_script_args(*args, **kwargs)
-
-    packaging.override_get_script_args = my_get_script_args
-    easy_install.get_script_args = my_get_script_args
-
-    # another hack to allow setup from tarball.
-    orig_get_version = packaging.get_version
-
-    def my_get_version(package_name, pre_version=None):
-        if package_name == 'ryu':
-            return str(version)
-        return orig_get_version(package_name, pre_version)
-
-    packaging.get_version = my_get_version
